227ce61c466960959cf78b0cd7ddbb280932cd08
Mauaqp <peraltilla93@hotmail.com>
2022-01-21 14:33:15 -0500

    Entrega Final

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+
 
 app = Flask(__name__)
+app.secret_key = "miclave"
 
+#Route
+@app.route('/')         
+def index():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] += 2
+    return render_template("index.html")
+
+#Route increment
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 #App.run
 if __name__ == "__main__":
